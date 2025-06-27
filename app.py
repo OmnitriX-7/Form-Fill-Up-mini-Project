6d62659c6b6cb4e1928e2167b4f24ef6fc851a31
OmnitriX-7 <phantomkiller365@gmail.com>
2025-06-27 20:24:51 +0530

    Added class MyForm

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
+
+class MyForm(Flask):
+    name = StringField('Name', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    remember_me = BooleanField('Remember me')
+    salary = DecimalField('Salary', validators=[InputRequired])
+    gender = RadioField('Gender', choices=[('male', 'Male'), ('female','Female')])
+    country = SelectField('Country', choices=[('IN', 'India'), ('US', 'United States'), ('UK', 'United Kingdom')])
+    message = TextAreaField('message', validators=[InputRequired()])
+    photo = FileField('Photo')
+    
+    
